0e901e1b8bc35c198a01346942bb08981f9fd5f2
jinsa <jinsa004@gmail.com>
2022-10-21 13:52:53 +0900

    DB쿼리 세팅완료

---
+++ src/main/java/site/metacoding/white/domain/board.java
@@ -0,0 +1,22 @@
+package site.metacoding.white.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 해당 db에 맞는 전략을 맞춤. ex)마리아디비는 시퀀스를 안쓰기때문에 이걸 설정해줘야 오토임플리먼트로 함
+    private Long id;
+    private String title;
+    @Column(length = 1000)
+    private String content;
+}
