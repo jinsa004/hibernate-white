24053a81d97fe05eab31b4619dd9091b9a740faa
jinsa <jinsa004@gmail.com>
2022-10-21 14:08:34 +0900

    domain Board 파일 대소문자 수정

--- src/main/java/site/metacoding/white/domain/board.java
+++ src/main/java/site/metacoding/white/domain/board.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @Entity
-public class board {
+public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 해당 db에 맞는 전략을 맞춤. ex)마리아디비는 시퀀스를 안쓰기때문에 이걸 설정해줘야 오토임플리먼트로 함
     private Long id;
